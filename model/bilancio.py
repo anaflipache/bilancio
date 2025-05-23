27e6d0e56237061e0433eedb0eb5c6aaae8b86fc
Ana Maria Flipache <anamariaflipache@icloud.com>
2025-05-23 10:12:13 +0200

    upload

---
+++ model/bilancio.py
@@ -0,0 +1,8 @@
+class Bilancio:
+
+    def __init__(self, id=None, data=None, descrizione=None, importo=None, tipo=None):
+        self.id = id
+        self.data = data
+        self.descrizione = descrizione
+        self.importo = importo
+        self.tipo = tipo
